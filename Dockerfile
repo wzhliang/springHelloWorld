45830d6b188dadd9f832c165a7d4363471a7d7f4
Wenzhi Liang <wenzhi.liang@me.com>
2016-09-22 11:48:27 +0800

    run -> cmd

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN mkdir -p /usr/src/myapp
 COPY target/gs-rest-service-0.1.0.jar /usr/src/myapp/
 WORKDIR /usr/src/myapp
 EXPOSE 8080
-RUN java -jar gs-rest-service-0.1.0.jar
+CMD java -jar gs-rest-service-0.1.0.jar
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --rm -v "$PWD":/usr/src/mymaven:rw -v ~/.m2:/root/.m2:rw -w /usr/src/mymaven maven:3-jdk-8 mvn clean package
+docker run -it --rm -v "$PWD":/usr/src/mymaven:rw -v ~/.m2:/root/.m2:rw -w /usr/src/mymaven maven:3-jdk-8 mvn clean package
